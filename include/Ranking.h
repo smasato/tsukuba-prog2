561ec7428b285fcb5d8201c317db3f7b48bf17b6
Masato Sugiyama <public@smasato.net>
2019-02-11 16:05:25 +0900

    Add Ranking

---
+++ include/Ranking.h
@@ -0,0 +1,36 @@
+//
+// include/Ranking.h
+//
+
+#ifndef __INCLUDE_RANKING_H__
+#define __INCLUDE_RANKING_H__
+
+typedef struct {
+    int score;
+    char name[20];
+} User;
+
+typedef struct {
+    int size;
+    User ranking[10];
+} Ranking;
+
+#define RANKING_DATA_CSV "./data/ranking_data.csv"
+
+void InitRanking(Ranking *ranking);
+
+int LoadRankingFile(char *filename, Ranking *ranking);
+
+int SaveRankingFile(char *filename, Ranking *ranking);
+
+int CanRankIn(Ranking *ranking, int score);
+
+int AddToRanking(Ranking *ranking, int score, char name[20]);
+
+void SortRanking(Ranking *ranking);
+
+void RenderRanking(Ranking *ranking);
+
+void PrintRanking(Ranking *ranking);
+
+#endif //__INCLUDE_RANKING_H__
---
+++ lib/Ranking.c
@@ -0,0 +1,90 @@
+//
+// lib/Ranking.h
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "Ranking.h"
+
+void InitRanking(Ranking *ranking) {
+    ranking->size = 0;
+}
+
+int LoadRankingFile(char *filename, Ranking *ranking) {
+    FILE *fp = fopen(filename, "r");
+    if (fp != NULL) {
+        int i;
+        for (i = 0; i < 10; i++) {
+            if (fscanf(fp, "%[^,],%d\n", ranking->ranking[i].name, &(ranking->ranking[i].score)) == EOF) {
+                break;
+            }
+        }
+        ranking->size = i;
+        fclose(fp);
+
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int SaveRankingFile(char *filename, Ranking *ranking) {
+    FILE *fp = fopen(filename, "w");
+    if (fp != NULL) {
+        for (int i = 0; i < ranking->size; i++) {
+            fprintf(fp, "%s,%d\n", ranking->ranking[i].name, ranking->ranking[i].score);
+        }
+
+        fclose(fp);
+
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int CanRankIn(Ranking *ranking, int score) {
+    if (ranking->size < 10) return ranking->size;
+    for (int i = 0; i < ranking->size; i++) {
+        if (ranking->ranking[i].score < score) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int AddToRanking(Ranking *ranking, int score, char name[20]) {
+    int i = CanRankIn(ranking, score);
+    if (i != -1) {
+        ranking->ranking[i].score = score;
+        strcpy(ranking->ranking[i].name, name);
+
+        ranking->size++;
+
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void SortRanking(Ranking *ranking) {
+    User tmp;
+    for (int i = 0; i < (ranking->size - 1); ++i) {
+        for (int j = (ranking->size - 1); j > i; j--) {
+            if (ranking->ranking[j - 1].score < ranking->ranking[j].score) {
+                tmp = ranking->ranking[j - 1];
+                ranking->ranking[j - 1] = ranking->ranking[j];
+                ranking->ranking[j] = tmp;
+            }
+        }
+    }
+}
+
+void RenderRanking(Ranking *ranking);
+
+void PrintRanking(Ranking *ranking) {
+    for (int i = 0; i < ranking->size; i++) {
+        printf("%s: %d\n", ranking->ranking[i].name, ranking->ranking[i].score);
+    }
+}
