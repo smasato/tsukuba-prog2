22fc76db95c94c13511f7726a751971ee8f19f95
Masato Sugiyama <public@smasato.net>
2019-02-16 16:17:01 +0900

    Update timer library

--- include/Timer.h
+++ include/Timer.h
@@ -5,11 +5,8 @@
 #ifndef __INCLUDE_TIMER_H__
 #define __INCLUDE_TIMER_H__
 
-void StartTimer(void);
-
 time_t GetTime(void);
 
 time_t GetRapTime(time_t prevTime);
 
 #endif //__INCLUDE_TIMER_H__
-
--- lib/Timer.c
+++ lib/Timer.c
@@ -3,17 +3,10 @@
 #include <sys/time.h>
 #include "Timer.h"
 
-static struct timespec ts;
-
-void StartTimer(void) {
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-}
-
 time_t GetTime(void) {
-    struct timespec newTs;
-    clock_gettime(CLOCK_MONOTONIC, &newTs);
-
-    return newTs.tv_sec - ts.tv_sec;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec;
 }
 
 time_t GetRapTime(time_t prevTime) {
