81469726f0bfc8f99a7d9bcb32085853e650ef1f
Masato Sugiyama <public@smasato.net>
2019-02-16 15:21:37 +0900

    Update

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "Texture.h"
 #include "Ranking.h"
+#include "Timer.h"
 
 #ifdef __APPLE__
 
@@ -19,15 +20,74 @@
 #define RANKING_INPUT 2
 #define RANKING 3
 
+#define MOLAR_G 0
+#define MOLAR_H 1
+#define MOLAR_V 2
+#define MOLAR_B 3
+#define MOLAR_N 4
+
 int g_Page = 0;
 
 int g_WindowWidth = 512;
 int g_WindowHeight = 512;
 
+unsigned char g_Key = 255;
+
+int g_Molar;
+
+double g_prevTime = 0, g_rapTime = 0;
+
 Texture g_Tex[5];
-Ranking ranking;
+Texture g_Molar_Tex[5];
+Ranking g_ranking;
+
+void keyboardHandlerInGame(unsigned char key) {
+    switch (key) {
+        case 'g':
+        case 'G':
+            g_Key = 0;
+            break;
+        case 'h':
+        case 'H':
+            g_Key = 1;
+            break;
+        case 'v':
+        case 'V':
+            g_Key = 2;
+            break;
+        case 'b':
+        case 'B':
+            g_Key = 3;
+            break;
+        case 'n':
+        case 'N':
+            g_Key = 4;
+            break;
+        default:
+            g_Key = 255;
+            break;
+    }
+}
+
+void game(void) {
+    if (g_rapTime == 0) {
+        StartTimer();
+        DrawTexture(&g_Tex[GAME], 0, 0, g_Tex[GAME].width, g_Tex[GAME].width);
+    }
+    g_prevTime = GetTime();
+
+    if (g_rapTime > 60) exit(0);
+
+    if (g_Molar == g_Key)g_Molar = rand() % 5;
+
+    DrawTexture(&g_Molar_Tex[g_Molar], 0, 0, g_Molar_Tex[g_Molar].width, g_Molar_Tex[g_Molar].height);
+
+    g_rapTime = GetRapTime(g_prevTime);
+}
 
 void init(void) {
+    srand((unsigned) time(NULL));
+
     glClearColor(0.0, 0.0, 0.0, 1.0);
 
     glOrtho(0, g_WindowWidth, g_WindowHeight, 0, -1, 1);
@@ -50,12 +110,13 @@ void display(void) {
             DrawTexture(&g_Tex[TITLE], 0, 0, g_Tex[TITLE].width, g_Tex[TITLE].width);
             break;
         case GAME:
+            game();
             break;
         case RANKING_INPUT:
             break;
         case RANKING:
             DrawTexture(&g_Tex[RANKING], 0, 0, g_Tex[RANKING].width, g_Tex[RANKING].width);
-            RenderRanking(&ranking);
+            RenderRanking(&g_ranking);
             break;
         default:
             break;
@@ -65,42 +126,46 @@ void display(void) {
 }
 
 void idle(void) {
-    glutPostRedisplay();
+    display();
 }
 
 void keyboard(unsigned char key, unsigned int x, unsigned int y) {
-    switch (key) {
-        case 'q':
-        case 'Q':
-        case '\033':
-            exit(0);
-        case 's':
-        case 'S':
-            switch (g_Page) {
-                case TITLE:
-                    g_Page = GAME;
-                    break;
-            }
-        case 'r':
-        case 'R':
-            switch (g_Page) {
-                case TITLE:
-                    g_Page = RANKING;
-                    break;
-            }
-            break;
-        case 't':
-        case 'T':
-            switch (g_Page) {
-                case TITLE:
-                    break;
-                case RANKING:
-                    g_Page = TITLE;
-                    break;
-            }
-            break;
+    if (g_Page == GAME) {
+        keyboardHandlerInGame(key);
+    } else {
+        switch (key) {
+            case 'q':
+            case 'Q':
+            case '\033':
+                exit(0);
+            case 's':
+            case 'S':
+                switch (g_Page) {
+                    case TITLE:
+                        g_Page = GAME;
+                        break;
+                }
+                break;
+            case 'r':
+            case 'R':
+                switch (g_Page) {
+                    case TITLE:
+                        g_Page = RANKING;
+                        break;
+                }
+                break;
+            case 't':
+            case 'T':
+                switch (g_Page) {
+                    case TITLE:
+                        break;
+                    case RANKING:
+                        g_Page = TITLE;
+                        break;
+                }
+                break;
+        }
     }
-
     glutPostRedisplay();
 }
 
